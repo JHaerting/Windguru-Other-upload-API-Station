004dc943c88c8349382f01505fd50cf2c11f0dd6
JHaerting <jakob.haerting@gmail.com>
2026-04-02 22:30:34 +0300

    Update sync_weather.py

--- sync_weather.py
+++ sync_weather.py
@@ -6,7 +6,7 @@ import os
 import json
 from datetime import datetime, timedelta
 
-STATIONS = [
+STATIONS = [ # make modifications below to add or remove weather stations.
     {"name": "Inari Kirakkajärvi", "fmisid": "102055", "wg_uid": os.environ.get('WG_UID_1'), "wg_pass": os.environ.get('WG_PASS_1')},
     {"name": "Inari Seitalaassa", "fmisid": "129963", "wg_uid": os.environ.get('WG_UID_2'), "wg_pass": os.environ.get('WG_PASS_2')},
     {"name": "Yyteri", "fmisid": "101267", "wg_uid": os.environ.get('WG_UID_3'), "wg_pass": os.environ.get('WG_PASS_3')}
